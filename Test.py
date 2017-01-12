4cb75638252fd0953b0d6e64c9f968d770e3165f
MariLiis <mariliisvelner@gmail.com>
2017-01-13 00:47:26 +0200

    Changes made

--- Test.py
+++ Test.py
@@ -5,69 +5,55 @@ import scipy.io as sp
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import cross_val_predict
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-import random
+import toolbox
 
-DATA_DIR = "<directory of Dog_X>"
-DOG = "X"
-
-
-os.chdir(DATA_DIR)
+DIRNAME = "C:/Users/Kaur/Documents/CompNeuro/Project/idata/Dog_2"
+os.chdir(DIRNAME)
+subject = 2
+preictals = 42
+interictals = 50
 
 """
-file = sp.loadmat('Dog_1_interictal_segment_0001.mat')
+file = sp.loadmat('Dog_2_interictal_segment_0001.mat')
 print(file.keys())
 #print(file)
 print(file['interictal_segment_1'][0][0]['sequence'])
 """
 
-# create list of files to use
-filelist = []
-# data=np.array([])
 print('adding preictal segment data')
 # loop 0
-filelist.append('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, 1))
-file = sp.loadmat('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, 1))
-data = np.array([file['preictal_segment_' + str(1)][0][0]['data']])
-for i in range(1, 24):
-    filelist.append('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    file = sp.loadmat('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    data = np.append(data, np.array([file['preictal_segment_' + str(i + 1)][0][0]['data']]), axis=0)
+file = sp.loadmat('Dog_{:1d}_preictal_segment_{:04d}.mat'.format(subject, 1))
+data = np.array([toolbox.extaggregate(toolbox.chaggregate(file['preictal_segment_' + str(1)][0][0]['data']))])
+for i in range(1, preictals):
+    file = sp.loadmat('Dog_{:1d}_preictal_segment_{:04d}.mat'.format(subject, i + 1))
+    data = np.append(data, np.array(
+        [toolbox.extaggregate(toolbox.chaggregate(file['preictal_segment_' + str(i + 1)][0][0]['data']))]), axis=0)
     # print(np.array([file['preictal_segment_'+str(i+1)][0][0]['data']]).shape)
     # print('ictal:',i+1)
 print(data.shape)
 print('adding interictal segment data')
-for i in range(100):  # 480):
-    filelist.append('Dog_{}_interictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    file = sp.loadmat('Dog_{}_interictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    data = np.append(data, np.array([file['interictal_segment_' + str(i + 1)][0][0]['data']]), axis=0)
+for i in range(interictals):  # 100):#480):
+    file = sp.loadmat('Dog_{:1d}_interictal_segment_{:04d}.mat'.format(subject, i + 1))
+    data = np.append(data, np.array(
+        [toolbox.extaggregate(toolbox.chaggregate(file['interictal_segment_' + str(i + 1)][0][0]['data']))]), axis=0)
     # orint(np.array([file['interictal_segment_'+str(i+1)][0][0]['data']]).shape)
     # print('inter: ',i+1)
 print(data.shape)
-x = data.shape[0]
-y = data.shape[1] * data.shape[2]
 
-
-data = data.reshape((x, y))  # make the data edible for classifiers expecting 2D array
+data = data.reshape(
+    (preictals + interictals, data.shape[1] * data.shape[2]))  # make the data edible for classifiers expecting 2D array
 print('data added')
 # create list of results: 0 means no seizure, 1 means seizure in 55-65 minutes
 # 2 means seizure in 45-55 minutes etc. up to 6 (5-15 minutes)
 print('adding target data')
-"""
-target=np.array([])
-for i in range(4):
-    for j in range(6):
-        np.append(target,j)
-        #this way we can not only predict whether there will be a seizure,
-        #but also when the seizure will take place
-for i in range(100):#480):
-    np.append(target,0)
-    """
-target = np.zeros(124)
-for i in range(4):
+
+target = np.zeros(preictals + interictals)
+for i in range(int(preictals / 6)):
     for j in range(6):  # distinguishes between time-to-seizure
-        target[i + j] = j
+        target[i * 6 + j] = j
 
 print('target data added')
 print(target.shape)
@@ -79,7 +65,7 @@ clfNB = GaussianNB()
 # linear Support Vector Classifier
 clflSVC = svm.SVC(kernel='linear', C=1)
 # ensemble SVC
-clfeSVC = svm.SVC(kernel='ensemble', C=1)
+clfeSVC = svm.SVC(kernel='sigmoid', C=1)
 # K-nearest
 clfK = KNeighborsClassifier(4)
 # Random Forest
@@ -88,12 +74,14 @@ clfRF = RandomForestClassifier(max_depth=10, n_estimators=10, max_features=3)
 
 # results:
 def score(clf, name):
-    score = cross_val_score(clf, data, target, cv=3)
-    print(name, score.mean(), '+-', score.std())
+    # score=cross_val_score(clf,data,target,cv=3)
+    # print(name,score.mean(),'+-',score.std())
+    score = cross_val_predict(clf, data, target, cv=3)
+    print(score)
 
 
 score(clfNB, "Naive Bayes")
 # score(clflSVC,"Linear SVC")
-# score(clfeSVC,"Ensemble SVC")
+# score(clfeSVC,"Sigmoid SVC")
 score(clfK, "K nearest")
 score(clfRF, "Random Forest")
--- Test3.py
+++ Test3.py
@@ -5,68 +5,55 @@ import scipy.io as sp
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from sklearn.cross_validation import cross_val_score
+from sklearn.cross_validation import cross_val_predict
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-import random
+import toolbox
 
-DATA_DIR = "<directory of Dog_X>"
-DOG = "X"
-
-
-os.chdir(DATA_DIR)
+DIRNAME = "C:/Users/Kaur/Documents/CompNeuro/Project/idata/Dog_2"
+os.chdir(DIRNAME)
+subject = 2
+preictals = 42
+interictals = 50
 
 """
-file = sp.loadmat('Dog_1_interictal_segment_0001.mat')
+file = sp.loadmat('Dog_2_interictal_segment_0001.mat')
 print(file.keys())
 #print(file)
 print(file['interictal_segment_1'][0][0]['sequence'])
 """
 
-# create list of files to use
-filelist = []
-# data=np.array([])
 print('adding preictal segment data')
 # loop 0
-filelist.append('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, 1))
-file = sp.loadmat('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, 1))
-data = np.array([file['preictal_segment_' + str(1)][0][0]['data']])
-for i in range(1, 24):
-    filelist.append('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    file = sp.loadmat('Dog_{}_preictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    data = np.append(data, np.array([file['preictal_segment_' + str(i + 1)][0][0]['data']]), axis=0)
+file = sp.loadmat('Dog_{:1d}_preictal_segment_{:04d}.mat'.format(subject, 1))
+data = np.array([toolbox.aggregate(file['preictal_segment_' + str(1)][0][0]['data'])])
+for i in range(1, preictals):
+    file = sp.loadmat('Dog_{:1d}_preictal_segment_{:04d}.mat'.format(subject, i + 1))
+    data = np.append(data, np.array([toolbox.aggregate(file['preictal_segment_' + str(i + 1)][0][0]['data'])]), axis=0)
     # print(np.array([file['preictal_segment_'+str(i+1)][0][0]['data']]).shape)
     # print('ictal:',i+1)
 print(data.shape)
 print('adding interictal segment data')
-for i in range(100):  # 480):
-    filelist.append('Dog_{}_interictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    file = sp.loadmat('Dog_{}_interictal_segment_{:04d}.mat'.format(DOG, i + 1))
-    data = np.append(data, np.array([file['interictal_segment_' + str(i + 1)][0][0]['data']]), axis=0)
+for i in range(interictals):  # 100):#480):
+    file = sp.loadmat('Dog_{:1d}_interictal_segment_{:04d}.mat'.format(subject, i + 1))
+    data = np.append(data, np.array([toolbox.aggregate(file['interictal_segment_' + str(i + 1)][0][0]['data'])]),
+                     axis=0)
     # orint(np.array([file['interictal_segment_'+str(i+1)][0][0]['data']]).shape)
     # print('inter: ',i+1)
 print(data.shape)
-x = data.shape[0]
-y = data.shape[1] * data.shape[2]
 
-data = data.reshape((x, y))  # make the data edible for classifiers expecting 2D array
+data = data.reshape(
+    (preictals + interictals, 1278752))  # 3836256)) #make the data edible for classifiers expecting 2D array
 print('data added')
-# create list of results: 0 means no seizure, 1 means seizure
+# create list of results: 0 means no seizure, 1 means seizure in 55-65 minutes
+# 2 means seizure in 45-55 minutes etc. up to 6 (5-15 minutes)
 print('adding target data')
-"""
-target=np.array([])
-for i in range(4):
-    for j in range(6):
-        np.append(target,j)
-        #this way we can not only predict whether there will be a seizure,
-        #but also when the seizure will take place
-for i in range(100):#480):
-    np.append(target,0)
-    """
-target = np.zeros(124)
-for i in range(4):
-    for j in range(6):  # seizure oncoming
-        if j >= 4:  # ignores distant premonitions
-            target[i + j] = 1
+
+target = np.zeros(preictals + interictals)
+for i in range(int(preictals / 6)):
+    for j in range(6):  # only knows seizure is coming within 5-35 minutes
+        if j >= 3:
+            target[i * 6 + j] = 1
 
 print('target data added')
 print(target.shape)
@@ -87,12 +74,14 @@ clfRF = RandomForestClassifier(max_depth=10, n_estimators=10, max_features=3)
 
 # results:
 def score(clf, name):
-    score = cross_val_score(clf, data, target, cv=3)
-    print(name, score.mean(), '+-', score.std())
+    # score=cross_val_score(clf,data,target,cv=3)
+    # print(name,score.mean(),'+-',score.std())
+    score = cross_val_predict(clf, data, target, cv=3)
+    print(score)
 
 
 score(clfNB, "Naive Bayes")
 score(clflSVC, "Linear SVC")
-score(clfeSVC, "Ensemble SVC")
+score(clfeSVC, "Sigmoid SVC")
 score(clfK, "K nearest")
 score(clfRF, "Random Forest")
